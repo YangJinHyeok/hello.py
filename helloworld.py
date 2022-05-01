823fb8a64232d6f6dda56e3b5ac78f74a19b1deb
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2022-05-02 00:23:37 +0900

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,14 +1,4 @@
-#Touched by dh
-
-
 opensource = "hello"
-sw_training = "wolrd"
-
 print(opensource)
-print(sw_training)
 
-name = "양진혁"
-studentId = "202004309"
-major = "컴퓨터,전자 시스템공학부"
-
-print(name, studentId, major)
+양진혁 202004309 컴퓨터,전자 시스템공학부
